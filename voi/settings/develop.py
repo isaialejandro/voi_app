5d200dcd8750424318832f903e72db64aed072e2
isai <isaialejandro26@gmail.com>
2020-04-03 21:59:38 -0600

    config develop mod.

---
+++ voi/settings/develop.py
@@ -0,0 +1,31 @@
+import os
+from .base import *
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['www.voiregadmin.com']
+
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+
+#Raspberry Local
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'voireg_db',
+        'HOST': '192.168.3.151',
+        'USER': 'voireg_admin',
+        'PORT': '5432',
+        'PASSWORD': 'mario bross 2',
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATIC_ROOT = '/home/isai/Django_projects/static'
+MEDIA_ROOT = '/home/isai/Django_projects/media'
--- voi/wsgi.py
+++ voi/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voi.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voi.settings.local')
-##os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voi.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voi.settings.develop')
 
 application = get_wsgi_application()
