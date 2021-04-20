14a554f24d0a7217184622b2e554c08776ab396b
Isaí Alejandro <isaialejandro26@gmail.com>
2021-04-20 14:28:53 -0500

    Renaming.

--- apps/tools/mixins.py
+++ apps/tools/mixins.py
@@ -30,29 +30,3 @@ class CSRFExemptMixin(object):
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super(CSRFExemptMixin, self).dispatch(*args, **kwargs)
-
-
-"""
-class TemplatePermissionMixin(object):
-
-    def dispatch(self, request, *args, **kwargs):
-        if not self.has_permissions():
-            raise Http404('You do not have permission.')
-        return super(TemplatePermissionMixin, self).dispatch(
-            request, *args, **kwargs)
-
-
-from braces.views import AccessMixin
-
-class SuperOrManagerPermissionsMixin(AccessMixin):
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated():
-            return self.handle_no_permission(request)
-        if self.user_has_permissions(request):
-            return super(SuperOrManagerPermissionsMixin, self).dispatch(
-                request, *args, **kwargs)
-        raise Http404 #or return self.handle_no_permission
-
-    def user_has_permissions(self, request):
-        return self.request.user.is_superuser or self.request.user.is_manage
-"""
